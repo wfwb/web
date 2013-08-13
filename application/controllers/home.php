03f26422b97ef4d4d114d68ce4317d1b22e4fd60
Witsarut Suwanich <l2ollingneko@gmail.com>
2013-08-13 23:09:53 +0700

    Add carousel banner :3 (uncomplete)

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -20,6 +20,7 @@ class Home extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('templates/header');
+		$this->load->view('home');
 	}
 }
 
---
+++ application/views/home.php
@@ -0,0 +1,19 @@
+	<div id="wfwb-carousel" class="carousel slide">
+		<ol id="indicators-middle" class="carousel-indicators">
+			<li data-target="#wfwb-carousel" data-slide-to="0" class="active"></li>
+			<li data-target="#wfwb-carousel" data-slide-to="1"></li>
+		</ol>	
+		<div class="carousel-inner">
+			<div class="active item">
+				<img src="<?php echo base_url()?>assets/img/ex-carousel-1.jpg">	
+			</div>
+			<div class="item">
+				<img src="<?php echo base_url()?>assets/img/ex-carousel-2.jpg">	
+			</div>	
+		</div>
+
+		<a class="carousel-control left" href="#wfwb-carousel" data-slide="prev" style="margin-top: 15px;">&lsaquo;</a>
+		<a class="carousel-control right" href="#wfwb-carousel" data-slide="next" style="margin-top: 15px;">&rsaquo;</a>
+	</div>
+
+</div> <!-- div page -->
